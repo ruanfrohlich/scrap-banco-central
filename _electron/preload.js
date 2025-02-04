24de9aef9ada41ad2343681969ceece99d27b545
Ruan Frohlich <ruanfrohlich@gmail.com>
2025-02-04 16:20:47 -0300

    Add _electron folder to .gitignore

--- _electron/preload.js
+++
@@ -1,2 +0,0 @@
-var e=require("electron");(0,e.contextBridge).exposeInMainWorld("electronAPI",{fetchBCB:r=>(0,e.ipcRenderer).invoke("fetchBCB",r),onUpdateCounter:r=>(0,e.ipcRenderer).on("update-counter",(e,n)=>r(n)),counterValue:r=>(0,e.ipcRenderer).send("counter-value",r)});
-//# sourceMappingURL=preload.js.map
